c7622c6b28e452a93ddd977c08208383391da1c0
thulebonagwala <thulebonagwala@gmail.com>
2025-06-14 05:29:28 +0200

    Add layout for authentication pages

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,6 +1,8 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Input from '../../component/Inputs/Input';
 import { Link } from "react-router-dom";
+import AuthLayout from '../../component/Layouts/AuthLayout';
+
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -26,43 +28,45 @@ const Login = () => {
         //Login API Call
     }
     return (
-        <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-            <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
-            <p className="text-xs text-slate-700 mt-[5px] mb-6">
-                Please enter your details to log in
-            </p>
-
-            <form onSubmit={handleLogin}>
-                <Input
-                    value={email}
-                    onChange={({ target }) => setEmail(target.value)}
-                    label="Email Address"
-                    placeholder="john@example.com"
-                    type="text"
-                />
-
-                <Input
-                    value={password}
-                    onChange={({ target }) => setPassword(target.value)}
-                    label="Password"
-                    placeholder="Min 8 Characters"
-                    type="password"
-                />
-
-                {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
-
-                <button type="submit" className="btn-primary">
-                    LOGIN
-                </button>
-
-                <p className="text-[13px] text-slate-800 mt-3">
-                    Don’t have an account?{" "}
-                    <Link className="font-medium text-primary underline" to="/signup">
-                        SignUp
-                    </Link>
+        <AuthLayout>
+            <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+                <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+                <p className="text-xs text-slate-700 mt-[5px] mb-6">
+                    Please enter your details to log in
                 </p>
-            </form>
-        </div>
+
+                <form onSubmit={handleLogin}>
+                    <Input
+                        value={email}
+                        onChange={({ target }) => setEmail(target.value)}
+                        label="Email Address"
+                        placeholder="john@example.com"
+                        type="text"
+                    />
+
+                    <Input
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)}
+                        label="Password"
+                        placeholder="Min 8 Characters"
+                        type="password"
+                    />
+
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+                    <button type="submit" className="btn-primary">
+                        LOGIN
+                    </button>
+
+                    <p className="text-[13px] text-slate-800 mt-3">
+                        Don’t have an account?{" "}
+                        <Link className="font-medium text-primary underline" to="/signup">
+                            SignUp
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </AuthLayout>
     )
 }
 
