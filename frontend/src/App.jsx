a1fafa137fc6046dee7f88b1241ced39a85d5a91
thulebonagwala <thulebonagwala@gmail.com>
2025-06-14 04:50:00 +0200

    Feat: Add Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Auth/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <div className='text-2xl'>
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
