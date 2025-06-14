1416213a1612c7904a8eb98825fdb707db7e1a6e
thulebonagwala <thulebonagwala@gmail.com>
2025-06-14 09:37:02 +0200

    Connect the server to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Start Server
 const PORT = process.env.PORT || 5000;
