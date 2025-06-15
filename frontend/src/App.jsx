525843a0e4be124e8969c90a414a48e146b8dad2
thulebonagwala <thulebonagwala@gmail.com>
2025-06-15 03:15:16 +0200

    Add authentication backgroud image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Auth/Login'
 import SignUp from './pages/Auth/SignUp'
