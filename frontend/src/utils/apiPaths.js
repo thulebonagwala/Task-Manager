8d8c67f9cd31e7cc1353903b28654b76974ecc33
thulebonagwala <thulebonagwala@gmail.com>
2025-06-15 05:09:19 +0200

    add user Context and api paths

---
+++ frontend/src/utils/apiPaths.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "http://localhost:8000";
+
+// utils/apiPaths.js
+export const API_PATHS = {
+  AUTH: {
+    REGISTER: "/api/auth/register", // Register a new user (Admin or Member)
+    GET_PROFILE: "/api/auth/profile", // Get logged-in user details
+  },
+};
