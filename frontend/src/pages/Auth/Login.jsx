2f2eb9719da3ed94c81bc01c24db1d7f806a5e48
thulebonagwala <thulebonagwala@gmail.com>
2025-06-16 10:30:55 +0200

    Intergrate login page with backend aunthentication

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,73 +1,106 @@
-import React, { useState } from 'react'
-import Input from '../../component/Inputs/Input';
-import { Link } from "react-router-dom";
-import AuthLayout from '../../component/Layouts/AuthLayout';
-
+import React, { useContext, useState } from "react";
+import AuthLayout from "../../component/Layouts/AuthLayout";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../component/Inputs/Input"
+import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { UserContext } from "../../context/userContext";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    // Handle Login Form Submit
-    const handleLogin = async (e) => {
-        e.preventDefault();
+  const {updateUser} = useContext(UserContext)
+  const navigate = useNavigate();
 
-        if (!validateEmail(email)) {
-            setError("Please enter a valid email address.");
-            return;
-        }
+  // Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-        if (!password) {
-            setError("Please enter the password");
-            return;
-        }
-
-        setError("");
-
-        //Login API Call
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
     }
-    return (
-        <AuthLayout>
-            <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-                <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
-                <p className="text-xs text-slate-700 mt-[5px] mb-6">
-                    Please enter your details to log in
-                </p>
 
-                <form onSubmit={handleLogin}>
-                    <Input
-                        value={email}
-                        onChange={({ target }) => setEmail(target.value)}
-                        label="Email Address"
-                        placeholder="john@example.com"
-                        type="text"
-                    />
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
 
-                    <Input
-                        value={password}
-                        onChange={({ target }) => setPassword(target.value)}
-                        label="Password"
-                        placeholder="Min 8 Characters"
-                        type="password"
-                    />
+    setError("");
 
-                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+    //Login API Call
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
 
-                    <button type="submit" className="btn-primary">
-                        LOGIN
-                    </button>
+      const { token, role } = response.data;
 
-                    <p className="text-[13px] text-slate-800 mt-3">
-                        Don’t have an account?{" "}
-                        <Link className="font-medium text-primary underline" to="/signup">
-                            SignUp
-                        </Link>
-                    </p>
-                </form>
-            </div>
-        </AuthLayout>
-    )
-}
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data)
 
-export default Login
+        //Redirect based on role
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }
+      }
+    } catch (error){
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don’t have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup">
+              SignUp
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -5,5 +5,6 @@ export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register", // Register a new user (Admin or Member)
     GET_PROFILE: "/api/auth/profile", // Get logged-in user details
+    LOGIN: "/api/auth/login", // Authenticate user & return JWT token
   },
 };
