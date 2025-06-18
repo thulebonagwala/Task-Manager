86db83cd1a59a16f3f98e50a4f325e0c6dc51e2f
thulebonagwala <thulebonagwala@gmail.com>
2025-06-18 22:10:17 +0200

    Edit task route to include all Task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 // Task Management Routes
 router.get("/user-dashboard-data", protect, getUserDashboardData);
+router.get("/", protect, getTasks); // Get all tasks (Admin: all, User: assigned)
 
 module.exports = router;
