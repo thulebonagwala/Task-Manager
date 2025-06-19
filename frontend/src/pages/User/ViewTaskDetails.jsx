51e74b453dd87db858d7a9cf9a9e94e5794129e5
thulebonagwala <thulebonagwala@gmail.com>
2025-06-19 13:45:35 +0200

    Edit viewTaskDetails to add infobox function

--- frontend/src/pages/User/ViewTaskDetails.jsx
+++ frontend/src/pages/User/ViewTaskDetails.jsx
@@ -173,3 +173,15 @@ const ViewTaskDetails = () => {
 };
 
 export default ViewTaskDetails;
+
+const InfoBox = ({ label, value }) => {
+  return (
+    <>
+      <label className="text-xs font-medium text-slate-500">{label}</label>
+
+      <p className="text-[12px] md:text-[13px] font-medium text-gray-700 mt-0.5">
+        {value}
+      </p>
+    </>
+  );
+};
