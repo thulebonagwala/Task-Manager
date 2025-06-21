5e47999a9193707ec1ac736117ac809a92a0b4a5
thulebonagwala <thulebonagwala@gmail.com>
2025-06-21 19:52:17 +0200

    Implement controlloer for updating task status

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -174,7 +174,35 @@ const deleteTask = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+// @desc    Update task status
+// @route   PUT /api/tasks/:id/status
+// @access  Private
+const updateTaskStatus = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
 
+    const isAssigned = task.assignedTo.some(
+      (userId) => userId.toString() === req.user._id.toString()
+    );
+
+    if (!isAssigned && req.user.role !== "admin") {
+      return res.status(403).json({ message: "Not authorized" });
+    }
+
+    task.status = req.body.status || task.status;
+
+    if (task.status === "Completed") {
+      task.todoChecklist.forEach((item) => (item.completed = true));
+      task.progress = 100;
+    }
+
+    await task.save();
+    res.json({ message: "Task status updated", task });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 // @desc    Dashboard Data (User-specific)
 // @route   GET /api/tasks/user-dashboard-data
 // @access  Private
