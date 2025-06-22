a5cb84acf4cc29138605c20fbdd681f014efa9c8
thulebonagwala <thulebonagwala@gmail.com>
2025-06-22 19:28:32 +0200

    Add route for getting user profile

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 // Auth Routes
 router.post("/register", registerUser);   // Register User
 router.post("/login", loginUser);         // Login User
+router.get("/profile", protect, getUserProfile);  // Get User Profile
 
 module.exports = router;
