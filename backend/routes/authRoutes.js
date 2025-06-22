5f4210d8f0c2434244ad0fcbc464806edc92bcd6
thulebonagwala <thulebonagwala@gmail.com>
2025-06-22 19:50:05 +0200

    Add route for posting profile picture

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,4 +9,14 @@ router.post("/login", loginUser);         // Login User
 router.get("/profile", protect, getUserProfile);  // Get User Profile
 router.put("/profile", protect, updateUserProfile); // Update Profile
 
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
+
 module.exports = router;
